1abc86657f44d5beb1c4d1b74dd944c8be243f43
Gregory Shimansky <gregory.shimansky@intel.com>
2021-05-24 13:35:53 -0500

    Disable CUDA in apache arrow build
    Signed-off-by: Gregory Shimansky <gregory.shimansky@intel.com>

--- scripts/common-functions.sh
+++ scripts/common-functions.sh
@@ -100,7 +100,7 @@ function install_arrow() {
     -DARROW_PARQUET=ON \
     -DARROW_FILESYSTEM=ON \
     -DARROW_S3=ON \
-    -DARROW_CUDA=ON \
+    -DARROW_CUDA=OFF \
     -DTHRIFT_HOME=${THRIFT_HOME:-$PREFIX} \
     ${ARROW_TSAN} \
     ..
